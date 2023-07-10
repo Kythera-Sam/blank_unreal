3131b26c82bbb2d3e934dcd71bdb4a5b742d50c9
Callum Morrison <callumm@kythera.ai>
2023-07-10 15:38:25 +0100

    first commit

---
+++ Source/UnrealBlankProject/UnrealBlankProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealBlankProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALBLANKPROJECT_API AUnrealBlankProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
